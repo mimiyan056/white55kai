227c52b71308f48480c2cca54828cd4ad2d7a493
hnliyuan <258620825@qq.com>
2017-04-19 12:39:16 +0800

    index header

--- white55kai/src/Home.js
+++ white55kai/src/Home.js
@@ -3,38 +3,40 @@
  */
 
 import React, { Component } from 'react'
-import { View, Text, StatusBar, TouchableHighlight, StyleSheet } from 'react-native'
-
+import { View, Text, StatusBar, TouchableHighlight, StyleSheet, Image } from 'react-native'
+import Icon from 'react-native-vector-icons/MaterialIcons'
+import Util from './Util'
+import { SearchBar } from 'react-native-elements'
 
 class Home extends Component{
 	
 	constructor() {
 		super()
 		this.state = {
-			translucent : true
+			width: Util.size.width
 		}
 	}
-	
-	
-	_onChangeTranslucent  = () => {
-	    this.setState({
-	      translucent: !this.state.translucent,
-	    });
-    }
 
 	render() {
 		return (
-		      <View>
+		      <View style={[styles.mainContainer,{ width:this.state.width }]}>
 		        <StatusBar
-		          translucent={this.state.translucent}
+		          backgroundColor='#d45f58'
 		        />
-		        <TouchableHighlight
-		          style={styles.wrapper}
-		          onPress={this._onChangeTranslucent}>
-		          <View style={styles.button}>
-		            <Text>translucent: {this.state.translucent ? 'true' : 'false'}</Text>
-		          </View>
-		        </TouchableHighlight>
+		        <View style={styles.searchHeader}>
+		        	<Icon color={'white'} name='local-movies' size={30}  containerStyle={{height:30}}/>
+		        	<Text style={{color:'white',fontSize:18,height:30,paddingTop:4}}>丑开影视</Text>
+		        	<View style={{height:30, width:this.state.width/2, marginLeft:15}}>
+		        		<Image
+					        style={[styles.icon,{ width:this.state.width/2 }]}
+					        source={require('./images/search.png')}
+				      	/>
+		        	</View>
+		        	<View style={{height:30, margin:5, marginLeft:10, marginTop:7}}>
+		        		<Icon color={'white'} name='history' size={20} />
+		        	</View>
+		        	
+		        </View>
 		      </View>
 			)
 	}
@@ -43,20 +45,21 @@ class Home extends Component{
 
 
 styles = StyleSheet.create({
-  wrapper: {
-    borderRadius: 5,
-    marginBottom: 5,
+  mainContainer: {
+    flex:1,
+    flexDirection: 'column'
   },
-  button: {
-    borderRadius: 5,
-    backgroundColor: '#eeeeee',
-    padding: 10,
+  searchHeader: {
+  	height:40,
+  	backgroundColor:'#d45f58',
+  	flexDirection: 'row',
+  	padding:5,
   },
-  title: {
-    marginTop: 16,
-    marginBottom: 8,
-    fontWeight: 'bold',
+  icon: {
+  	height:20,
+  	marginTop:7
   }
+  
 });
 
 export default Home
